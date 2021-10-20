b94db9c96618dc87559e300cc92d876711551e46
AnanyaHD <91865296+AnanyaHD@users.noreply.github.com>
2021-10-20 22:44:22 +0530

    Add files via upload

---
+++ qualicoach/src/main/java/assignment/qualicoach/App.java
@@ -0,0 +1,12 @@
+package assignment.qualicoach;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
